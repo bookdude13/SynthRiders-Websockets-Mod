44c8bbdfb4ff37e7d1e818c9110bd4b94e752b03
bookdude13 <rfredlund13@gmail.com>
2025-01-08 11:34:04 -0600

    Fix warning, get SRModCore with bumped version

--- SynthRidersWebsockets/TaskExtensions.cs
+++ SynthRidersWebsockets/TaskExtensions.cs
@@ -10,7 +10,7 @@ namespace SynthRidersWebsockets
     // https://stackoverflow.com/questions/30490140/how-to-work-with-system-net-websockets-without-asp-net
     public static class TaskExtensions
     {
-        public static async Task<T?> WithCancellationToken<T>(this Task<T> source, CancellationToken cancellationToken)
+        public static async Task<T> WithCancellationToken<T>(this Task<T> source, CancellationToken cancellationToken)
         {
             var cancellationTask = new TaskCompletionSource<bool>();
             cancellationToken.Register(() => cancellationTask.SetCanceled());
